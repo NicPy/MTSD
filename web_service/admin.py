4af3a5637aba9082b42ac95269899f05b8105aa0
NicPy <illiterteone@yandex.ru>
2018-05-06 21:33:21 +0300

    Added a StaticPage model, where will be about and contact us pages located

--- web_service/admin.py
+++ web_service/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from .models import News, NewsCategory, AboutInfo, Project
+from .models import News, NewsCategory, AboutInfo, Project, StaticPage
 
 admin.site.register(News)
 admin.site.register(NewsCategory)
 admin.site.register(AboutInfo)
 admin.site.register(Project)
+admin.site.register(StaticPage)
--- web_service/models.py
+++ web_service/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class NewsCategory(models.Model):
@@ -21,4 +22,10 @@ class AboutInfo(models.Model):
 class Project(models.Model):
     item_title = models.CharField(max_length=110)
     item_text = models.TextField(max_length=3000)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     pub_date = models.DateTimeField('date published')
+
+class StaticPage(models.Model):
+    slug = models.CharField(max_length=60, unique=True)
+    item_title = models.CharField(max_length=110)
+    item_text = models.TextField(max_length=3000)
