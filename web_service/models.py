dde866c810d35a5b1eb86e277ae7c2d4542429a1
NicPy <allmotofb@gmail.com>
2018-05-07 19:42:54 +0300

    About  page model has been deleted

--- web_service/models.py
+++ web_service/models.py
@@ -13,12 +13,6 @@ class News(models.Model):
     published = models.BooleanField(default = True)
     category = models.ForeignKey(NewsCategory, on_delete=models.CASCADE)
 
-
-class AboutInfo(models.Model):
-    item_title = models.CharField(max_length=110)
-    item_text = models.CharField(max_length=3000)
-    pub_date = models.DateTimeField('date published')
-
 class Project(models.Model):
     item_title = models.CharField(max_length=110)
     item_text = models.TextField(max_length=3000)
@@ -29,3 +23,4 @@ class StaticPage(models.Model):
     slug = models.CharField(max_length=60, unique=True)
     item_title = models.CharField(max_length=110)
     item_text = models.TextField(max_length=3000)
+    pub_date = models.DateTimeField('date published')
