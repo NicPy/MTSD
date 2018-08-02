3db0fa1e9109c9eaea7158ad732a988aa1b094e4
NicPy <illiterteone@yandex.ru>
2018-08-02 19:01:16 +0300

    added profile simple view

--- web_service/forms.py
+++ web_service/forms.py
@@ -18,16 +18,17 @@ class UserCreateForm(UserCreationForm):
         return user
 
 class Profile(forms.ModelForm):
-    country = forms.CharField()
-    city = forms.CharField()
-    help_with   = forms.CharField(widget = forms.Textarea)
-    need_help_with = forms.CharField(widget = forms.Textarea)
-    contact_info = forms.CharField(widget=forms.Textarea)
-    photo = forms.ImageField()
+    country = forms.CharField(required = False)
+    city = forms.CharField(required = False)
+    work_place = forms.CharField(required = False)
+    help_with   = forms.CharField(widget = forms.Textarea, required = False)
+    need_help_with = forms.CharField(widget = forms.Textarea, required = False)
+    contact_info = forms.CharField(widget=forms.Textarea, required = False)
+    photo = forms.ImageField(required = False)
 
     class Meta:
         model = UserProfile
-        fields = ("country", "city", "work_place", "help_with", "need_help_with", "contact_info", "age", "photo")
+        fields = ("country", "city", "work_place", "help_with", "need_help_with", "contact_info", "photo")
 
 
     # def save(self, commit=True):
